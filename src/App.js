e73836a7cf013d0adefe2e0a781146e4d2395f5f
vinics <vinics.dr@gmail.com>
2020-07-14 12:37:19 -0300

    Solution upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  // States
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  // Methods
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories');
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const { status } = await api.delete(`repositories/${id}`);
+    
+    if (status === 204) {
+      const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+      const updatedRepos = [...repositories];
+      updatedRepos.splice(repoIndex, 1);
+
+      setRepositories(updatedRepos);
+    }
+
+    // const repoIndex = repositories.findIndex(repo => repo.id === id);
+    
+    // console.log(repoIndex);
+    // console.log(repositories.splice(repoIndex, 1));
+    
+    // setRepositories(update);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo => <li key={repo.id}>{repo.title}
+        
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
